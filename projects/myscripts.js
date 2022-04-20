e3aabbd5e754e5e4e62ce1c27e7fa63a59f3a7f8
mmm18m <mmm18m@my.fsu.edu>
2022-04-20 18:16:58 -0400

    d

--- projects/myscripts.js
+++ projects/myscripts.js
@@ -19,29 +19,26 @@ var projectIndex = 0;
 showProjects();
 
 function showProjects() {
-  var i;
-  var slides = document.getElementsByClassName("slides")
   var slide1 = document.getElementById("slide1")
   var slide2 = document.getElementById("slide2")
   var slide3 = document.getElementById("slide3")
 
   if (window.location.href == "https://mary-meberg.github.io/projects/index.html#no-js-slider-1") {
   console.log("match") 
-  slide2.style.visibility='hidden'
-    slide3.style.visibility='hidden' 
+  slide2.style.visibility="hidden"
+    slide3.style.visibility="hidden"
   } 
   
   if (window.location.href == "https://mary-meberg.github.io/projects/index.html#no-js-slider-2"){
     console.log("match")  
-  slide1.style.visibility='hidden'
-    slide3.style.visibility='hidden' 
+  slide1.style.visibility="hidden"
+    slide3.style.visibility="hidden" 
   }
 
   if (window.location.href == "https://mary-meberg.github.io/projects/index.html#no-js-slider-3"){
     console.log("match")  
-  slide1.style.visibility='hidden'
-    slide2.style.visibility='hidden' 
+  slide1.style.visibility="hidden"
+    slide2.style.visibility="hidden"
   }
-
 
 }
